7c97a9ab610752676a4fe6675d86e3886a1299af
Jinyi <jinyi.chen@mail.utoronto.ca>
2018-10-19 15:22:31 -0400

    aws_keys

---
+++ shell_scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#install bottle
+ #$ cd <YOUR-PROJECT-DIRECTORY>
+ #$ wget https://pypi.python.org/packages/source/b/bottle/bottle-0.12.7.tar.gz
+ #$ tar -zxvf bottle-0.12.7.tar.gz
+ #$ cd bottle-0.12.7
+ #$ python setup.py install --user
+ 
+#install module:
+#oauth2client: 
+pip install --upgrade oauth2client
+#beaker.middleware: 
+pip install beaker
+#Google API client: (this should also install    httplib2, if not: pip install httplib2)
+pip install google-api-clinet
+
+#
