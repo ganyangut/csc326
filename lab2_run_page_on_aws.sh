a41dc953b0f03facdf7d929f9f2c436bbdee621c
ganyang <gabriel.yang@mail.utoronto.ca>
2018-11-13 23:29:16 -0500

    changed run_aws scripts

--- lab2_run_page_on_aws.sh
+++ lab2_run_page_on_aws.sh
@@ -14,11 +14,11 @@ while IFS='' read -r public_ip || [[ -n "$public_ip" ]]
 do
     echo "Public IP: $public_ip"
     # copy the code to the instance
-    #scp -i waldoge_key_pair.pem -r copy_to_aws/lab1_code ubuntu@${public_ip}:~/
-    #scp -i waldoge_key_pair.pem copy_to_aws/setup_lab1.sh ubuntu@${public_ip}:~/
+    scp -i waldoge_key_pair.pem -r copy_to_aws/lab1_code ubuntu@${public_ip}:~/
+    scp -i waldoge_key_pair.pem copy_to_aws/setup_lab1.sh ubuntu@${public_ip}:~/
 
     # setup environment and run the page on aws
-    ssh -i waldoge_key_pair.pem ubuntu@${public_ip} 'nohup ./setup_lab1.sh'
+    ssh -i waldoge_key_pair.pem ubuntu@${public_ip} "screen && source setup_lab1.sh"
 done < "public_ip.txt"
 
 cd ..
